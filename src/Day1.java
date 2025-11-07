0ad199914e2742f5e12bb0cb079156f3016d996b
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-07 11:58:03 +0000

    Day1

---
+++ src/Day1.java
@@ -0,0 +1,15 @@
+public class Day1 {
+
+
+    public static void main(String[] args) {
+        SumOfTwoNumbers();
+    }
+
+    public static void SumOfTwoNumbers(){
+        int a = 2,b=4;
+        int sum;
+        sum = a+b;
+        System.out.println("The sum of two numbers are 8kk " +sum);
+
+    }
+}
