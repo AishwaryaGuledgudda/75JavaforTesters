5ee1f46149ef53232a5b02f71b078683a769c6b5
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-07 16:04:00 +0000

    Day3

---
+++ src/Day3.java
@@ -0,0 +1,9 @@
+public class Day3 {
+
+    public static void main(String[] args) {
+        int a = 8;
+        if(a%2==0){
+            System.out.println("Even");
+        }else System.out.println("Odd");
+    }
+}
