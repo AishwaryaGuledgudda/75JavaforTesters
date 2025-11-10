48ad17a4d1ded39aacb4bb649fc2e65627303526
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-10 16:35:37 +0000

    miscellaneous

--- src/Day3.java
+++ src/Day3.java
@@ -1,7 +1,7 @@
 public class Day3 {
 
     public static void main(String[] args) {
-        int a = 8;
+        int a = 88;
         if(a%2==0){
             System.out.println("Even");
         }else System.out.println("Odd");
---
+++ src/Kitty.java
@@ -0,0 +1,6 @@
+public class Kitty {
+
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
