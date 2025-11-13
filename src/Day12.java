d114241dbba5a0e153a3e772d5213471e537615d
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-13 16:56:19 +0000

    Day12

---
+++ src/Day12.java
@@ -0,0 +1,12 @@
+public class Day12 {
+    public static void main(String[] args) {
+        int num = 1;
+        int count = 0;
+
+        while(num!=0){
+            num = num / 10;
+            count++;
+        }
+        System.out.println("The number of digits are "+count);
+    }
+}
