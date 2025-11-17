88ef77613a7f6cca5c14d0e034b1cc0d9c035982
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-17 16:16:30 +0000

    Day13

---
+++ src/day13.java
@@ -0,0 +1,9 @@
+public class day13 {
+    public static void main(String[] args) {
+        int year = 2028;
+
+        if((year % 4==0 && year%100!=0)|| (year%400==0)){
+            System.out.println("Is a leap year");
+        } else System.out.println("Not a leap year");
+    }
+}
