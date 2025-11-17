6111e302a0d00da5fdf77c9d01d62e61714c99e5
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-17 16:16:35 +0000

    Day14

---
+++ src/Day14.java
@@ -0,0 +1,10 @@
+public class Day14 {
+    public static void main(String[] args) {
+        int num = 5;
+
+        for(int i=1;i<=10;i++){
+            int mul = num*i;
+            System.out.println(num + "x" + i + "=" +mul);
+        }
+    }
+}
