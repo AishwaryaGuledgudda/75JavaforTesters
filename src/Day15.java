9981ea8ccef66c3f847e2fee036e90025708934c
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-20 11:18:29 +0000

    Day14

---
+++ src/Day15.java
@@ -0,0 +1,7 @@
+public class Day15 {
+    public static void main(String[] args) {
+        char ch = 'B';
+        int Ascii = (int) ch;
+        System.out.println("The Ascii value is " +Ascii);
+    }
+}
