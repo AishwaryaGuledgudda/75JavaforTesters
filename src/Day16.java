a64183722a6a5e6914242297d8bf55a453ce0b95
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-21 18:16:25 +0000

    Day16

---
+++ src/Day16.java
@@ -0,0 +1,9 @@
+public class Day16 {
+    public static void main(String[] args) {
+        int N=10;
+        int Sum;
+
+        Sum = N *(N+1)/2;
+        System.out.println("The sum of N numbers are " +Sum);
+    }
+}
