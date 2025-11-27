53bc1482d0c6a0279832227327f213ef25370e9d
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-11-27 11:51:45 +0000

    Day19

---
+++ src/Day19.java
@@ -0,0 +1,15 @@
+public class Day19 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int n = a.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            int temp = a[i];
+            a[i] = a[n - 1 - i];
+            a[n - 1 - i] = temp;
+        }
+        for (int arr : a) {
+            System.out.print(arr + " ");
+        }
+    }
+}
