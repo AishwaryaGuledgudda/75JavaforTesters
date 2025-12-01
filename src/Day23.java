95bf3fa253d38c3b552427cae3e3c607185f6812
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-12-01 10:45:23 +0000

    Day23

---
+++ src/Day23.java
@@ -0,0 +1,14 @@
+public class Day23 {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        str = str.toLowerCase();
+        int count=0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+        System.out.println("Num of vowels present are: " +count);
+    }
+}
