f938721610c16500337c62bc3b4efb7e4351460d
Aishwarya Guledgudda <aishwaryaguledgudda@gmail.com>
2025-12-02 12:45:09 +0000

    Day26

---
+++ src/Day26.java
@@ -0,0 +1,8 @@
+public class Day26 {
+    public static void main(String[] args) {
+        String str = "Ezy QA Verse";
+        String result = str.replaceAll("\\s+","");
+        System.out.println("The string without spaces are: " +result);
+    }
+
+}
